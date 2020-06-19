2ee97a05409772bafd020d5354f106d0b9b8c398
Luca <mail@lucamegens.nl>
2020-06-19 14:56:34 +0200

    Interactive media verbeterd, design, research, Prof.Id en comm
    Ik heb aantal leeruitkomsten verbeterd en extra tekst en uitleg toegevoegd naar aanleiding van feedback

--- InterMedia.js
+++ InterMedia.js
@@ -26,3 +26,34 @@ function IMshowSlides(IMn) {
   IMslides[IMslideIndex-1].style.display = "block";
   IMdots[IMslideIndex-1].className += " IMactive";
 }
+
+
+
+var IMslideIndex1 = 1;
+IMshowSlides1(IMslideIndex1);
+
+// Next/previous controls
+function IMplusSlides1(IMn1) {
+    IMshowSlides1(IMslideIndex1 += IMn1);
+}
+
+// Thumbnail image controls
+function IMcurrentSlide1(IMn1) {
+    IMshowSlides1(IMslideIndex1 = IMn1);
+}
+
+function IMshowSlides1(IMn1) {
+  var i;
+  var IMslides1 = document.getElementsByClassName("IMSlides1");
+  var IMdots1 = document.getElementsByClassName("IMdot1");
+  if (IMn1 > IMslides1.length) {IMslideIndex1 = 1}
+  if (IMn1 < 1) {IMslideIndex1 = IMslides1.length}
+  for (i = 0; i < IMslides1.length; i++) {
+    IMslides1[i].style.display = "none";
+  }
+  for (i = 0; i < IMdots1.length; i++) {
+    IMdots1[i].className = IMdots1[i].className.replace(" IMactive", "");
+  }
+  IMslides1[IMslideIndex1-1].style.display = "block";
+  IMdots1[IMslideIndex1-1].className += " IMactive";
+}
