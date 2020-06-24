0f78231e78fac02350fb83638497803b09f50581
Luca <mail@lucamegens.nl>
2020-06-24 15:30:03 +0200

    Lettertype bugs verwijdert, titel kleur aangepast Reflectie toegevoegd

--- index2.js
+++ index2.js
@@ -1,18 +1,18 @@
 var i = 0;
-var images1 = [];
-var time1 = 5000;
+var images = [];
+var time = 3000;
 
 //image list
-images1[0] = 'POC/POC2/image1.png';
-images1[1] = 'POC/POC2/image2.png';
-images1[2] = 'POC/POC2/image3.jpg';
+images[0] = 'image1.png';
+images[1] = 'image2.png';
+images[2] = 'image3.jpg';
 
 //change image
 
-function changeImg1(){
-    document.slide.src = images1[i];
+function changeImg(){
+    document.slide.src = images[i];
 
-    if (i < images1.length - 1) 
+    if (i < images.length - 1) 
     {
         i++;  
     } 
@@ -21,8 +21,8 @@ function changeImg1(){
     {
         i = 0;
     }
-setTimeout1("changeImg1()" , time1);
+setTimeout("changeImg()" , time);
 
 }
 
-window.onload = changeImg1;
+window.onload = changeImg;
